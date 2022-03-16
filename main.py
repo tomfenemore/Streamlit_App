b5c2b061e262accddee91ea22ff2b3b44b641e9a
tomfenemore <tomfenemore@hotmail.co.uk>
2022-03-16 23:31:07 +0000

    move to st and logic done

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-import pandas as pd
-import numpy as np
-from plotly_calplot import calplot
 
-def cal(df):
-    dummy_start_date = "2019-01-01"
-    dummy_end_date = "2021-10-03"
 
-    dummy_df = pd.DataFrame({
-        "ds": pd.date_range(dummy_start_date, dummy_end_date),
-        "value": np.random.randint(low=0, high=30,
-        size=(pd.to_datetime(dummy_end_date) - pd.to_datetime(dummy_start_date)).days + 1,),
-        })
-    fig = calplot(
-        dummy_df,
-        x="ds",
-        y="value"
-    )
-    fig.show()
 
 
 if __name__ == '__main__':
-    cal(1)
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- st.py
+++ st.py
@@ -3,19 +3,21 @@ import trelloAPI as tr
 import pandas as pd
 import plotly.express as px
 import calendar
+import datetime
 
-df = pd.read_pickle('pic')
-#cal = calendar.cal()
+df = pd.read_pickle('data')
 venue = list(df['Venue'].unique())
 venue.append('All')
 wind_dir = ['N', 'NE','E', 'SE', 'S', 'SW', 'W', 'NW']
-wind_str = ['0-7knts', '7-12knts', '12-15knts', '15-18knts', '18-23knts']
+wind_str = ['0-7knts', '7-12knts', '12-15knts', '15-18knts', '18-23knts', '23-27knts','27+knts']
 
 st.title('Dais Condition Analysis')
 
 a = st.sidebar.selectbox('Mode', ['Condition Review', 'Condition Logging', 'Training Calendar'])
 if a == 'Condition Review':
+    rnge = st.sidebar.slider('How Long Ago?', value=datetime.date.today(), max_value=datetime.date.today(), min_value=min(df['Date']))
     ven = st.sidebar.selectbox('Venue', venue)
+    df = df[df['Date'] >= rnge]
     for i in venue:
         if ven == i:
             if ven != 'All':
@@ -23,25 +25,30 @@ if a == 'Condition Review':
             st.header(ven)
             diag = st.sidebar.selectbox('Diagram', ['Wind Strength', 'Wind Direction', 'Notes'])
             if diag == 'Wind Strength':
-                fig = px.line_polar(df, theta=df['Wind'].value_counts().index, r=df['Wind'].value_counts(), line_close=True, category_orders={ 'r' : ['0-7knts', '7-12knts', '12-15knts', '15-18knts', '18-23knts']}) #
+                fig = px.line_polar(df, theta=df['Wind Strength'].value_counts().index, r=df['Wind Strength'].value_counts(), line_close=True, category_orders={ 'r' : ['0-7knts', '7-12knts', '12-15knts', '15-18knts', '18-23knts']}) #
                 st.plotly_chart(fig, use_container_width=True)
 
             if diag == 'Wind Direction':
-                fig = px.line_polar(df, theta=df['Direction'].value_counts().index, r=df['Direction'].value_counts(), line_close=True, category_orders={ 'r' : ['N', 'NE','E', 'SE', 'S', 'SW', 'W', 'NW']}) #
+                fig = px.line_polar(df, theta=df['Wind Direction'].value_counts().index, r=df['Wind Direction'].value_counts(), line_close=True, category_orders={ 'r' : ['N', 'NE','E', 'SE', 'S', 'SW', 'W', 'NW']}) #
                 st.plotly_chart(fig, use_container_width=True)
 
             if diag == 'Notes':
                 st.subheader('Venue Notes')
                 dir = st.select_slider('Wind Direction', options=wind_dir)
-                str = st.select_slider('Wind Strength', options=wind_str, value=('0-7knts','18-23knts'))
-                #for i in df.rows:
-                with st.expander('Date'):
-                    st.write("some note written for the venue in question. More logic is needed")
-                with st.expander('Date'):
-                    st.write("some note written for the venue in question. More logic is needed")
+                df = df[df['Wind Direction'] == dir]
+                stren = st.select_slider('Wind Strength', options=wind_str)
+                df = df[df['Wind Strength'] == stren]
+                df.sort_values(by='Date', ascending=False, inplace=True)
+                for i in range(len(df.index)):
+                    with st.expander(df.iloc[i]['Name']+',  ('+str(df.iloc[i]['Date'])+')'):
+                        st.write(df.iloc[i]['Notes'])
+                        st.write(df.iloc[i]['Video'])
+
+
 
 if a == 'Condition Logging':
     st.header('Condition Logging')
+    name = st.text_input('Session Name')
     str = st.select_slider('Wind Strength', options=wind_str)
     col1, col2 = st.columns(2)
     with col1:
@@ -56,19 +63,23 @@ if a == 'Condition Logging':
 
     notes = st.text_area('Add notes')
 
+    video = st.text_input('Add Video Link')
+
     if st.button('Add Training Session'):
         st.write('Wind Strength', str)
         st.write('Wind Direction', dir)
         st.write('Date', date)
         st.write('Venue', ven)
         st.write('Notes', notes)
+        st.write(type(date))
 
         try:
             data = pd.read_pickle('data')
         except:
             data = pd.DataFrame()
-        data = data.append({'Wind Strength': [str], 'Wind Direction': [dir], 'Date': [date], 'Venue': [ven], 'Notes': [notes]}, ignore_index=True)
+        data = data.append({'Name':name, 'Wind Strength': str, 'Wind Direction': dir, 'Date': date, 'Venue': ven, 'Notes': notes, 'Video':video}, ignore_index=True)
         data.to_pickle('data')
+
     else:
         st.write('Press button to log a training session')
 
